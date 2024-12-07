c980d972615bafeeef3daa6c9a479b183f23b576
Taisa0413 <asajin1013@gmail.com>
2024-12-07 13:34:17 +0900

    課題指摘修正

--- kadai_018/src/kadai_018/Kato_Chapter18.java
+++ kadai_018/src/kadai_018/Kato_Chapter18.java
@@ -1,15 +1,15 @@
 package kadai_018;
 
 public abstract class  Kato_Chapter18 {
-public String familyName;
+     public String familyName = "加藤";
 	
 	 public String givenName;
 	
-	 public String address;
+	 public String address = "東京都中野区〇×";
 	 
-	public void commonIntroduce() {
-		 familyName = "加藤";
-		 address = "東京都中野区〇×";
+	 public void commonIntroduce() {
+		 System.out.println("名前は" + familyName + givenName + "です");
+	     System.out.println("住所は" + address + "です");
 	}
 	
 	public abstract void setGivenName();
@@ -18,8 +18,7 @@ public String familyName;
 	 
 	public void execIntroduce() {
 		 commonIntroduce(); 
-		 System.out.println("名前は" + familyName + givenName + "です");
-	     System.out.println("住所は" + address + "です");
 	     eachIntroduce(); 
+	     System.out.println();
 	}
 }
